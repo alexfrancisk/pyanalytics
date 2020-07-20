b33a89bb76c5c171d1c34b582d4f6db89513ff82
alexfrancisk <alexfrancisk@gmail.com>
2020-07-20 17:42:03 +0530

    123

--- file1.py
+++ file1.py
@@ -9,7 +9,15 @@ list1=[1,2,3]
 list1
 print(line1)
 # -*- coding: utf-8 -*-
+Spyder Editor
+
+This is a temporary script file.
 """
+
+list1=[1,2,3]
+list1
+print(line1)
+
 Spyder Editor
 
 This is a temporary script file.
