b93300368f9cda8faa4ecef4833c7918b3d8b703
alexfrancisk <alexfrancisk@gmail.com>
2020-07-20 17:17:36 +0530

    dhiraj

--- file1.py
+++ file1.py
@@ -8,3 +8,13 @@ This is a temporary script file.
 list1=[1,2,3]
 list1
 print(line1)
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+list1=[1,2,3]
+list1
+print(line1)
